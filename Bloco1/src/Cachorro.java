4f337667a067d5cc284c486d9649bf2522da4cb5
Hanna <79318688+Honey-lee429@users.noreply.github.com>
2021-06-09 13:42:22 -0300

    Projeto Bloco 1

---
+++ Bloco1/src/Cachorro.java
@@ -0,0 +1,23 @@
+
+public class Cachorro extends ClassePrincipal{
+private String castrado;
+	
+	public Cachorro(String nome, int idade, double peso, String porte, String genero, String castrado) {
+		super(nome, idade, peso, porte, genero);
+		this.castrado = castrado; 
+	}
+	
+	public void mostrarInfo() {
+		System.out.println("Nome do Cachorro: " + getNome());
+	}
+
+	public String getCastrado() {
+		return castrado;
+	}
+
+	public void setCastrado(String castrado) {
+		this.castrado = castrado;
+	}
+	
+	//criar método calcular quantidade de ração
+}
---
+++ Bloco1/src/TesteCachorro.java
@@ -0,0 +1,8 @@
+
+public class TesteCachorro {
+	public static void main(String[] args) {
+		Cachorro gui = new Cachorro("Gui", 7, 18, "médio", "macho", "sim");
+		gui.mostrarInfo();
+
+	}
+}
